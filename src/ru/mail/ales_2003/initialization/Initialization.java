82513631fa69cd2e2de292f4b0915177604e5838
admin <admin@DESKTOP-4D7AQF3>
2016-10-16 00:25:40 +0300

    https://github.com/Ales2003/Initialization.git

---
+++ src/ru/mail/ales_2003/initialization/Initialization.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ru.mail.ales_2003.initialization;
+
+/**
+ *
+ * @author admin
+ */
+public class Initialization {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        ChildClass childClass = new ChildClass();
+    }
+    
+}
